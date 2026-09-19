ca6956472f96121bc6b084d2f6fc3463479d3b6c
Lea Rosema <lea@lea.lgbt>
2026-09-19 11:53:00 +0200

    fix: add globals package for eslint, delete unnecessary jsconfig

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,18 @@ import { defineConfig } from 'eslint/config';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
+import { browser } from 'globals';
+
 export default defineConfig(
   { ignores: ['dist', 'site', 'coverage'] },
   eslint.configs.recommended,
   tseslint.configs.recommended,
   {
+    languageOptions: {
+			globals: {
+				...browser,
+			},
+		},
     rules: {
       '@typescript-eslint/ban-ts-comment': 'warn',
       '@typescript-eslint/no-empty-function': 'warn',
